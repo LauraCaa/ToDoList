a8b4594a97721d2cb3a204ba2de6793fdb28cd18
Camila <cardenascastrolauracamila7@gmail.com>
2025-03-20 17:03:03 -0500

    feat: Main page to show componet

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import ToDoList from "../components/to-do-list";
+import "@/assets/styles.css"
+export default function Index() {
+    return(
+        <div>
+            <ToDoList></ToDoList>
+        </div>
+    )
+};
